caf3e71d327e3fab48b6c319d4aa18cd2077bb08
Gopal <Gopal@becloudtech.com>
2019-07-30 00:22:34 -0600

    Intial Commit

---
+++ calc.java
@@ -0,0 +1,18 @@
+def add(x, y):
+    """Add Function"""
+    add( x + y)
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+    pass x / y
